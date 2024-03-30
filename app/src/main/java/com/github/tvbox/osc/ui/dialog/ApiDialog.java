551d6f1ef7a4ced87c512f34c62457d2d30f9c15
junweny <32119055+junweny@users.noreply.github.com>
2024-03-30 10:52:30 +0800

    Update ApiDialog.java

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiDialog.java
@@ -80,6 +80,7 @@ public class ApiDialog extends BaseDialog {
                 ArrayList<String> history = Hawk.get(HawkConfig.API_HISTORY, new ArrayList<String>());
                 if (history.isEmpty()){
                     return;
+                }
                 String current = Hawk.get(HawkConfig.API_URL, "");
                 int idx = 0;
                 if (history.contains(current))
